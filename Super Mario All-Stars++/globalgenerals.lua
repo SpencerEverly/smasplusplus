ac021a01758074fb9c02c9cfbcd9258c3e7737f0
Spencer Everly <spencer.everly@gmail.com>
2022-03-14 10:35:35 -0500

    Hang on, let me test on the actual engine

--- Super Mario All-Stars++/globalgenerals.lua
+++ Super Mario All-Stars++/globalgenerals.lua
@@ -185,9 +185,9 @@ function globalgenerals.onCameraUpdate(c, camIdx)
 end
 	
 function globalgenerals.onTick()
-	Misc.SetEngineSpeed(0.46)
-	Misc.SetEngineTPS(20.5)
-	Misc.SetEngineTickDuration(20.5)
+	--Misc.SetEngineSpeed(0.46)
+	--Misc.SetEngineTPS(20.5)
+	--Misc.SetEngineTickDuration(20.5)
 	local costumes = playerManager.getCostumes(player.character)
 	local currentCostume = player:getCostume()
 	
