bc967e4f4ca8213b89db8a0a869f3fa00b824336
Spencer Everly <spencer.everly@gmail.com>
2022-03-15 10:45:25 -0500

    Hopefully this'll show the hardcoded stuffs

--- Super Mario All-Stars++/costumes/mario/A2XT-Demo/costume.lua
+++ Super Mario All-Stars++/costumes/mario/A2XT-Demo/costume.lua
@@ -65,20 +65,20 @@ function costume.onInit(p)
 	Audio.sounds[75].sfx = Audio.SfxOpen("costumes/mario/A2XT-Demo/smb2-throw.ogg")
 	Audio.sounds[76].sfx = Audio.SfxOpen("costumes/mario/A2XT-Demo/smb2-hit.ogg")
 	Audio.sounds[91].sfx = Audio.SfxOpen("costumes/mario/A2XT-Demo/bubble.ogg")
-	Graphics.sprites.hardcoded[48-0].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Container0.png")
-	Graphics.sprites.hardcoded[48-1].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Container1.png")
-	Graphics.sprites.hardcoded[33-0].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface0.png")
-	Graphics.sprites.hardcoded[33-1].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface1.png")
-	Graphics.sprites.hardcoded[33-2].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface2.png")
-	Graphics.sprites.hardcoded[33-3].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface3.png")
-	Graphics.sprites.hardcoded[33-5].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface5.png")
-	Graphics.sprites.hardcoded[33-6].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface6.png")
-	Graphics.sprites.hardcoded[33-7].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface7.png")
-	Graphics.sprites.hardcoded[33-8].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface8.png")
-	Graphics.sprites.hardcoded[26-2].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Mount.png")
-	Graphics.sprites.hardcoded[25-1].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Boot1.png")
-	Graphics.sprites.hardcoded[25-2].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Boot2.png")
-	Graphics.sprites.hardcoded[25-3].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Boot3.png")
+	Graphics.sprites.hardcoded["48-0"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Container0.png")
+	Graphics.sprites.hardcoded["48-1"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Container1.png")
+	Graphics.sprites.hardcoded["33-0"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface0.png")
+	Graphics.sprites.hardcoded["33-1"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface1.png")
+	Graphics.sprites.hardcoded["33-2"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface2.png")
+	Graphics.sprites.hardcoded["33-3"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface3.png")
+	Graphics.sprites.hardcoded["33-5"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface5.png")
+	Graphics.sprites.hardcoded["33-6"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface6.png")
+	Graphics.sprites.hardcoded["33-7"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface7.png")
+	Graphics.sprites.hardcoded["33-8"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/interface8.png")
+	Graphics.sprites.hardcoded["26-2"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Mount.png")
+	Graphics.sprites.hardcoded["25-1"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Boot1.png")
+	Graphics.sprites.hardcoded["25-2"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Boot2.png")
+	Graphics.sprites.hardcoded["25-3"].img = Graphics.loadImageResolved("graphics/AdventuresOfDemo/ui/Boot3.png")
 end
 
 function costume.onCleanup(p)
@@ -161,20 +161,20 @@ function costume.onCleanup(p)
 	extrasounds.playersoundid101 = Audio.SfxOpen(Misc.resolveSoundFile("sound/dragon-coin-get4.ogg"))
 	extrasounds.playersoundid102 = Audio.SfxOpen(Misc.resolveSoundFile("sound/dragon-coin-get5.ogg"))
 	extrasounds.playersoundid103 = Audio.SfxOpen(Misc.resolveSoundFile("sound/cherry.ogg"))
-	Graphics.sprites.hardcoded[48-0].img = nil
-	Graphics.sprites.hardcoded[48-1].img = nil
-	Graphics.sprites.hardcoded[33-0].img = nil
-	Graphics.sprites.hardcoded[33-1].img = nil
-	Graphics.sprites.hardcoded[33-2].img = nil
-	Graphics.sprites.hardcoded[33-3].img = nil
-	Graphics.sprites.hardcoded[33-5].img = nil
-	Graphics.sprites.hardcoded[33-6].img = nil
-	Graphics.sprites.hardcoded[33-7].img = nil
-	Graphics.sprites.hardcoded[33-8].img = nil
-	Graphics.sprites.hardcoded[26-2].img = nil
-	Graphics.sprites.hardcoded[25-1].img = nil
-	Graphics.sprites.hardcoded[25-2].img = nil
-	Graphics.sprites.hardcoded[25-3].img = nil
+	Graphics.sprites.hardcoded["48-0"].img = nil
+	Graphics.sprites.hardcoded["48-1"].img = nil
+	Graphics.sprites.hardcoded["33-0"].img = nil
+	Graphics.sprites.hardcoded["33-1"].img = nil
+	Graphics.sprites.hardcoded["33-2"].img = nil
+	Graphics.sprites.hardcoded["33-3"].img = nil
+	Graphics.sprites.hardcoded["33-5"].img = nil
+	Graphics.sprites.hardcoded["33-6"].img = nil
+	Graphics.sprites.hardcoded["33-7"].img = nil
+	Graphics.sprites.hardcoded["33-8"].img = nil
+	Graphics.sprites.hardcoded["26-2"].img = nil
+	Graphics.sprites.hardcoded["25-1"].img = nil
+	Graphics.sprites.hardcoded["25-2"].img = nil
+	Graphics.sprites.hardcoded["25-3"].img = nil
 end
 
 return costume
