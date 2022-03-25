2e0199f20a7085400e213954b3656bf4c1088598
P-Star7 <P-Star7@users.noreply.github.com>
2022-03-24 19:52:25 -0500

    Costumes - Mega Man - Dr. Wily

---
+++ Super Mario All-Stars++/costumes/megaman/Dr. Wily/costume.lua
@@ -0,0 +1,14 @@
+local costume = {}
+
+function costume.onInit()
+	registerEvent(costume, "onDraw");
+end
+
+function costume.onDraw()
+	for _,v in ipairs(Animation.get(149)) do
+		v.width = 54;
+		v.height = 50;
+	end
+end
+
+return costume;
