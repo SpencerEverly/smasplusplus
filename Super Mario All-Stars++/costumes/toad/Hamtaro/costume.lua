90a840a760fea14377a24a41d6ae28e5d3594b4f
P-Star7 <P-Star7@users.noreply.github.com>
2022-03-24 19:46:48 -0500

    Costumes - Toad - Hamtaro

---
+++ Super Mario All-Stars++/costumes/toad/Hamtaro/costume.lua
@@ -0,0 +1,14 @@
+local costume = {}
+
+function costume.onInit()
+	registerEvent(costume, "onDraw");
+end
+
+function costume.onDraw()
+	for _,v in ipairs(Animation.get(130)) do
+		v.width = 74;
+		v.height = 60;
+	end
+end
+
+return costume;
