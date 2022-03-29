ff8c901ac9f44a2b9512bd656e4cbe66d912ed88
Spencer Everly <spencer.everly@gmail.com>
2022-03-29 10:49:34 -0500

    Wrong ID was used, now finally corrected

--- Super Mario All-Stars++/SMBS - W-1, L-2/luna.lua
+++ Super Mario All-Stars++/SMBS - W-1, L-2/luna.lua
@@ -2,9 +2,9 @@ local level_dependencies_normal= require("level_dependencies_normal")
 
 function onEvent(eventName)
 	if eventName == "Boss Start" then
-		Audio.MusicChange(1, "_OST/Super Mario Bros Spencer/Boss Battle.ogg")
+		Audio.MusicChange(0, "_OST/Super Mario Bros Spencer/Boss Battle.ogg")
 	end
 	if eventName == "Boss End" then
-		Audio.MusicChange(1, "_OST/Super Mario Bros Spencer/Athletic.ogg")
+		Audio.MusicChange(0, "_OST/Super Mario Bros Spencer/Athletic.ogg")
 	end
 end
