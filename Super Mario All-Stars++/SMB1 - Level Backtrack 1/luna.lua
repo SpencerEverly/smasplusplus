f8082d185f0db3b02cd8d0792e1411673c03c60d
Spencer Everly <spencer.everly@gmail.com>
2022-04-08 16:03:30 -0500

    Started World 3

--- Super Mario All-Stars++/SMB1 - Level Backtrack 1/luna.lua
+++ Super Mario All-Stars++/SMB1 - Level Backtrack 1/luna.lua
@@ -7,5 +7,8 @@ function onExit()
 	elseif player:mem(0x15E, FIELD_WORD) == 2 and player.forcedState == FORCEDSTATE_INVISIBLE then --SMB1 World 2
 		SaveData.smwMap.playerX = -196480
 		SaveData.smwMap.playerY = -199264
+	elseif player:mem(0x15E, FIELD_WORD) == 3 and player.forcedState == FORCEDSTATE_INVISIBLE then --SMB1 World 3
+		SaveData.smwMap.playerX = -195776
+		SaveData.smwMap.playerY = -199072
 	end
 end
