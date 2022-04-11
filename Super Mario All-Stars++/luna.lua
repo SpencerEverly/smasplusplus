37250996b9abb3fff3e6e2b66b42c3a09870d72c
Spencer Everly <spencer.everly@gmail.com>
2022-04-11 11:31:13 -0500

    Updated simple commands, also did some comments

--- Super Mario All-Stars++/luna.lua
+++ Super Mario All-Stars++/luna.lua
@@ -1,7 +1,16 @@
+--SUPER MARIO ALL-STARS++
+--By Spencer Everly
+--Here's the starting code that loads it all. How'd I do?
+
+--First thing before we get started is that we require the most important libraries on the top.
+
 local smwMap = require("smwMap")
 local classicEvents = require("classiceventsmod")
 local EventManager = require("main_events_mod")
-local function anyValidFields() --This is to prevent any player2 errors while switching between 1/2 player modes. If it's still not working then paste what's below into data/scripts/base/darkness.lua at line 854 and save. Hopefully this'll be fixed in the next patch, along with the teleporting issue
+
+--Then we fix up some functions that the X2 team didn't fix yet (If they released a patch and fixed a certain thing, the code will be removed from here).
+
+local function anyValidFields() --This is to prevent any player2 errors while switching between 1/2 player modes. If it's still not working (Hopefully that's not the case) then paste what's below into data/scripts/base/darkness.lua at line 854 and save. Hopefully this'll be fixed in the next patch, along with the teleporting issue
 	sectionlist[1] = player.section
 	if player2 and player2.isValid then
 		sectionlist[2] = player2.section
@@ -10,7 +19,7 @@ local function anyValidFields() --This is to prevent any player2 errors while sw
 	end
 end
 
-function Player:teleport(x, y, bottomCenterAligned) --Fixing 2nd player teleporting
+function Player:teleport(x, y, bottomCenterAligned) --This fixes 2nd player teleporting, when using player/player2:teleport.
 	-- If using bottom center aligned coordinates, handle that sensibly
 	if bottomCenterAligned then
 		x = x - (player.width * 0.5)
@@ -27,7 +36,7 @@ function Player:teleport(x, y, bottomCenterAligned) --Fixing 2nd player teleport
 	end
 end
 
-local serializer = require("ext/serializer")
+local serializer = require("ext/serializer") --We will then detect to see if the broken SMBX launcher has launched the game. If so, it'll prevent us from loading the episode and recommend us to launch using the SMBX2 launcher instead.
 local function loadSaveSlot(slot)
 	local filename = "save"..slot.."-ext.dat"
 	local f = io.open(Misc.episodePath():gsub([[[\/]+]], [[/]])..filename, "r")
@@ -39,13 +48,15 @@ local function loadSaveSlot(slot)
 			if s then
 				return e
 			else
-				pcall(Misc.dialog, "Error loading SaveData information. Your save file may be corrupted, or you launched the broken X2 launcher. Please seek assistance on the Codehaus Discord server (https://discord.gg/usMKuKF7SN), repairing your save data if you know how, or start a new game.\n\n=============\n"..e)
+				pcall(Misc.dialog, "Error loading SaveData information. Your save file may be corrupted, or you launched the broken SMBX launcher. Please seek assistance on the Codehaus Discord server (https://discord.gg/usMKuKF7SN), repairing your save data if you know how, or start a new game.\n\n=============\n"..e)
 			end
 		end
 		return {}
 	end
 	return {}
 end
+
+--Next up is some new resolveFile requiring functions, for simplifying the functions:
 
 function FileOpen(name) --This will not only check the main SMBX2 folders, but will also check for other common SMAS++ directories
 	return Misc.resolveFile(name)
@@ -56,43 +67,102 @@ function FileOpen(name) --This will not only check the main SMBX2 folders, but w
 		or Misc.resolveFile("___MainUserDirectory/" .. name)
 end
 
-function GraphicImage(name)
+function GraphicImageOpen(name) --Opening the graphics as easy as Pie!
 	local file = FileOpen(name) or FileOpen(name..".png")
 	if file then
-		return Graphics.loadImageResolved(FileOpen)
+		Graphics.loadImageResolved(FileOpen)
 	end
 	return nil
 end
 
-function SFXPlay(name)
-	return SFX.play(name)
+function GraphicImageDraw(name, xdraw, ydraw, opacitydraw, prioritydraw) --Drawing graphics got a lot better.
+	local file = FileOpen(name) or FileOpen(name..".png")
+	local xdraw = 0
+	local ydraw = 0
+	local opacitydraw = 1
+	if file then
+		Graphics.drawImageWP(name, xdraw, ydraw, opacitydraw, prioritydraw)
+	end
+	return nil
 end
 
-function MusicPlay(name, sectionid)
+function openSFX(name) --Opening SFXs
+	SFX.open(name)
+end
+
+function SFX(name) --Playing SFXs
+	SFX.play(name)
+end
+
+function Music(name, sectionid) --Music changing is now a LOT easier
 	if sectionid == -1 then
 		for i = 0,20 do
-			i = sectionid
+			sectionid = i
+		end
+	end
+	return Audio.MusicChange(sectionid, name)
+end
+
+function MusicAlt(name, sectionid) --Alternative music changer, for no reason
+	if sectionid == -1 then
+		for i = 0,20 do
+			sectionid = i
 		end
 	end
 	return Section(sectionid).music = name
 end
 
-Misc.saveGame()
-local globalgenerals = require("globalgenerals")
-local repll = require("repll")
-local rng = require("base/rng")
-local playerManager = require("playermanager")
+--Now we get to the Hub date detection stuff. First, we start with Easter...
 
-if SMBX_VERSION == VER_SEE_MOD then
+function easter (year) --"Happy easter! (Foot goes into the toilet with a kick sound)" - Vince
+    local G = year % 19
+    local C = div(year, 100)
+    local H = (C - div(C, 4) - div((8 * C + 13), 25) + 19 * G + 15) % 30
+    local I = H - div(H, 28) * (1 - div(29, H + 1)) * (div(21 - G, 11))
+    local J = (year + div(year, 4) + I + 2 - C + div(C, 4)) % 7
+    local L = I - J
+    local month = 3 + div(L + 40, 44)
+    return month, L + 28 - 31 * div(month, 4)
+end
+
+function holidays (year) --Some other misc. Easter stuff
+    local dates = {}
+    dates.easter = Time.date(year, easter(year))
+    dates.ascension = dates.easter + Time.days(39)
+    dates.pentecost = dates.easter + Time.days(49)
+    dates.trinity   = dates.easter + Time.days(56)
+    dates.corpus    = dates.easter + Time.days(60)
+    return dates
+end
+
+function isLeapYear(y) --Now for the leap year detection...
+    return y % 4 == 0 and y % 100 ~= 0 or y % 400 == 0
+end
+
+local years = {}
+local startD, endD = 1, 2020
+for i = startD, endD do
+  if isLeapYear(i) then years[#years + 1] = i end
+end
+--Note: You can just plainly use 'isLeapYear()' if you just want to check if a year is a leap year!
+
+--Now that everything has been loaded, save the game and start loading the medium important stuff
+Misc.saveGame()
+local globalgenerals = require("globalgenerals") --Most important library of all. This loads general stuff for levels.
+local repll = require("repll") --Custom sound command line, for testing in the editor
+local rng = require("base/rng") --Load up rng for etc. things
+local playerManager = require("playermanager") --Load up this to change Ultimate Rinka and Ninja Bomberman to Steve and Yoshi
+
+if SMBX_VERSION == VER_SEE_MOD then --SEE Mod stuff
 	Graphics.sprites.block[647].img = Graphics.loadImageResolved("graphics/seemod/block/block-647.png")
 	Graphics.sprites.block[648].img = Graphics.loadImageResolved("graphics/seemod/block/block-648.png")
 	Graphics.sprites.block[655].img = Graphics.loadImageResolved("graphics/seemod/block/block-655.png")
 	Graphics.sprites.block[656].img = Graphics.loadImageResolved("graphics/seemod/block/block-656.png")
 	princessRinka = require("characters/princessrinka")
 	Misc.setWindowTitle("Super Mario All-Stars++")
-	__customPauseMenuActive = true
+	__disablePauseMenu = true
 	Misc.setWindowIcon(Graphics.loadImageResolved("graphics/icon/icon.png"))
-elseif SMBX_VERSION <= VER_BETA4_PATCH_4_1 then
+elseif SMBX_VERSION <= VER_BETA4_PATCH_4_1 then --This will load things for any other SMB2 version
 	loadactivate = true
 	steve = require("steve")
 	playerManager.overrideCharacterLib(CHARACTER_ULTIMATERINKA,require("steve"))
@@ -109,6 +179,7 @@ elseif SMBX_VERSION <= VER_BETA4_PATCH_4_1 then
 	Graphics.sprites.ultimaterinka[player.powerup].img = Graphics.loadImageResolved("graphics/smbx2og/character/ultimaterinka-2.png")
 end
 
+--First time SaveData setting, for resolutions and other settings
 if SaveData.resolution == nil then
 	SaveData.resolution = "fullscreen"
 end
@@ -119,10 +190,10 @@ if SaveData.borderEnabled == nil then
 	SaveData.borderEnabled = true
 end
 
--- Gets the index of the player that the camera belongs to. A return value of 0 means that it belongs to everybody
+--Camera stuff to prevent player2 from doing split screen...
+--Gets the index of the player that the camera belongs to. A return value of 0 means that it belongs to everybody
 function onCameraUpdate(c, camIdx)
     local screenType = mem(0x00B25130,FIELD_WORD)
-
     if camera2.isSplit or screenType == 6 then -- split screen or supermario2 is active
         return camIdx
     elseif screenType == 5 then -- dynamic screen
@@ -140,14 +211,18 @@ function onCameraUpdate(c, camIdx)
     end
 end
 
-local loadingsoundFile = Misc.resolveSoundFile("loadscreen.ogg")
+--Now load the loading sound file!
+local loadingsoundFile = openSFX("loadscreen.ogg")
+
+--Placing in levels onto a table that'll prevent the loading sound from playing
 local noloadingsounds = {
 	["SMAS - Start.lvlx"] = true,
 	["SMAS - Raca's World (Part 0).lvlx"] = true,
 	["SMAS - Raca's World (Part 1).lvlx"] = true,
-	["SMAS - Map.lvlx"] = true
+	["map.lvlx"] = true
 }
 
+--Friendly place table for the SEE Mod's Princess Rinka
 local friendlyplaces = {
 	["SMAS - Start.lvlx"] = true,
 	["SMAS - Map.lvlx"] = true,
@@ -182,6 +257,7 @@ local friendlyplaces = {
 	["SMB1 - Level Backtrack 5.lvlx"] = true,
 	["SMB1 - Level Backtrack 6.lvlx"] = true,
 	["SMB1 - Level Backtrack 7.lvlx"] = true,
+	["SMB2 - Ending.lvlx"] = true,
 	["SMB2 - Level Backtrack 1.lvlx"] = true,
 	["SMB2 - Level Backtrack 2.lvlx"] = true,
 	["SMB2 - Level Backtrack 3.lvlx"] = true,
@@ -201,16 +277,18 @@ local friendlyplaces = {
 	["SMW - W-0, L-Blue Switch.lvlx"] = true
 }
 
+--Now use onLoad to play the loading sound...
 function onLoad()
-	if not Misc.inEditor() and loadactivate == true then --If luna errors during testing, this will be useful to not load the audio to prevent the audio from still being played until terminated --or (noloadingsounds[Level.filename()]) == true then
+	if not Misc.inEditor() or (Level.filename()[noloadingsounds]) == true and loadactivate == true then --If luna errors during testing in the editor, this will be useful to not load the audio to prevent the audio from still being played until the engine is terminated
 		loadingsoundchunk = Audio.SfxOpen(loadingsoundFile)
 		loadingSoundObject = Audio.SfxPlayObj(loadingsoundchunk, -1)
 		fadetolevel = true
 	end
 end
 
-function onStart()
-	loadSaveSlot(Misc.saveSlot())
+function onStart() --Now do onStart...
+	loadSaveSlot(Misc.saveSlot()) --This will load the save slot twice, to check to make sure it's been properly loaded
+	--Do the weather SaveData additions
 	if SaveData.dateplayedweather == nil then
 		SaveData.dateplayedweather = weatherControl
 	end
@@ -220,14 +298,11 @@ function onStart()
 	if SaveData.dateplayedday == nil then
 		SaveData.dateplayedday = os.date("%d")
 	end
-	if (friendlyplaces[Level.filename()]) == true then
+	if (Level.filename()[friendlyplaces]) == true then
 		if SMBX_VERSION == VER_SEE_MOD then
-			princessRinka.friendlyArea = true
+			princessRinka.friendlyArea = true --Set this to prevent Princess Rinka from getting killed in places such as the boot screen, intro, or the Hub
 		end
 	end
-	--if __LUNALUA == "0.7 SEE Mod" then
-		--GameData.fadetolevel = false
-	--end
 	if SaveData.dateplayedyesterday == nil then
 		yesterdaynumber = os.date("*t").day - 1
 		yesterdaystring = tostring(yesterdaynumber)
@@ -238,13 +313,13 @@ function onStart()
 		tomorrowstring = tostring(tomorrownumber)
 		SaveData.dateplayedtomorrow = tomorrowstring
 	end
-	if GameData.weatherset == true then
+	if GameData.weatherset == true then --This'll be better in the future. For now, it just loads the weather when restarting the game to the start preboot screen, or starting it up every time
 		possibleWeather = {"sunny","rain","snow"}
 		weatherControl = rng.randomEntry(possibleWeather)
 		SaveData.dateplayedweather = weatherControl --Write in a better onetime day function for this
 		GameData.weatherset = false
 	end
-	if not Misc.inEditor() or (Level.filename() == noloadingsounds[Level.filename()]) == true then
+	if not Misc.inEditor() or (Level.filename() == noloadingsounds[Level.filename()]) == true then --Make sure to fade out the loading sound when onStart is active...
 		fadetolevel = false
 		if loadactivate == true then
 			loadingSoundObject:FadeOut(800)
@@ -259,37 +334,14 @@ function onStart()
 		SaveData.dateplayedyear = os.date("%Y")
 	end
 	SaveData.dateplayedyear = os.date("%Y")
-	if SaveData.disableX2char == 0 then --Migrate old saves if there are any
+	if SaveData.disableX2char == 0 then --Migrate old saves from pre-March 2022 if there are any.
 		SaveData.disableX2char = false
 	end
 	if SaveData.disableX2char == 1 then
 		SaveData.disableX2char = true
 	end
-	if GameData.cutsceneMusicControl == false then
-		Audio.MusicVolume(65)
-	end
-	if GameData.cutsceneMusicControl == true then
-		Audio.MusicVolume(0)
-	end
-	--if __LUNALUA == "0.7 SEE Mod" then
-		--if GameData.fadetolevel == false then
-			--GameData.loadingSoundObject:FadeOut(800)
-		--end
-	---else
+	Audio.MusicVolume(nil) --Reset the music volume on onStart, just in case
 end
-
-function isLeapYear(y)
-    return y % 4 == 0 and y % 100 ~= 0 or y % 400 == 0
-end
-
-local years = {}
-local startD, endD = 1, 2020
-for i = startD, endD do
-  if isLeapYear(i) then years[#years + 1] = i end
-end
---[[
-  Note: You Can just plainly use 'isLeapYear()' if you just want to check if a year is a leap year!
-]]--
 
 function tomorrowget()
 	tomorrownumber = os.date("*t").day + 1
@@ -303,16 +355,8 @@ function yesterdayget()
 	SaveData.dateplayedyesterday = yesterdaystring
 end
 
-function onTick()
+function onTick() --This will prevent split screen, again (Just in case)
 	mem(0x00B25130,FIELD_WORD, 2)
 end
 
-function onExit()
-	--if __LUNALUA == "0.7 SEE Mod" then
-		--if not Misc.inEditor() then --If luna errors during testing, this will be useful to not load the audio to prevent the audio from still being played until terminated
-			--loadingsoundchunk = Audio.SfxOpen(loadingsoundFile)
-			--GameData.loadingSoundObject = Audio.SfxPlayObj(loadingsoundchunk, -1)
-			--GameData.fadetolevel = true
-		--end
-	--end
-end
+--That's the end of this file!
