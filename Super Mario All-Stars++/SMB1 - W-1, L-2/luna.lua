caf3113376d03700239567962a7bc8164e17c363
Spencer Everly <spencer.everly@gmail.com>
2022-04-14 09:30:23 -0500

    Finished SMB1 entirely, with setting warp zones! SMB2's warp central was also put in, fix coming soon

--- Super Mario All-Stars++/SMB1 - W-1, L-2/luna.lua
+++ Super Mario All-Stars++/SMB1 - W-1, L-2/luna.lua
@@ -5,4 +5,12 @@ function onExit()
 		SaveData.smwMap.playerX = -196480
 		SaveData.smwMap.playerY = -199264
 	end
+	if player:mem(0x15E, FIELD_WORD) == 5 and player.forcedState == FORCEDSTATE_INVISIBLE then --SMB1 World 3
+		SaveData.smwMap.playerX = -195776
+		SaveData.smwMap.playerY = -199072
+	end
+	if player:mem(0x15E, FIELD_WORD) == 5 and player.forcedState == FORCEDSTATE_INVISIBLE then --SMB1 World 4
+		SaveData.smwMap.playerX = -194944
+		SaveData.smwMap.playerY = -199136
+	end
 end
