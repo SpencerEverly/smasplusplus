fa4d5e1d59e4e9473193e28a9bbb594b547c92fb
Spencer Everly <spencer.everly@gmail.com>
2022-04-21 15:48:17 -0500

    Added SMB2 World 5-3 cutscene, explaining Wart's secret weakness

--- Super Mario All-Stars++/SMB2 - W-5, L-3/luna.lua
+++ Super Mario All-Stars++/SMB2 - W-5, L-3/luna.lua
@@ -1,8 +1,88 @@
 local level_dependencies_normal= require("level_dependencies_normal")
 
 function onStart()
-local character = player.character;
-  if (character == CHARACTER_YOSHI) then
-    triggerEvent("YoshiBossAddition")
+	local character = player.character
+	if (character == CHARACTER_YOSHI) then
+		triggerEvent("YoshiBossAddition")
+	end
+end
+
+local nokeys = false
+local nokeysexceptjump = false
+
+function onEvent(eventName)
+	if eventName == "Boss End (Part 2)" then
+		nokeys = true
+	end
+	if eventName == "Boss End 2 (Part 2)" then
+		playSound("smb2-world-clear.ogg")
+	end
+	if eventName == "Boss End 3 (Part 2)" then
+		nokeys = false
+	end
+	if eventName == "World Clear 1" then
+		nokeys = true
+	end
+	if eventName == "World Clear 2" then
+		playSound(4)
+	end
+	if eventName == "World Clear 4" then
+		playSound("record-scratch.ogg")
+		nokeys = false
+		nokeysexceptjump = true
+	end
+	if eventName == "World Clear 8" then
+		nokeysexceptjump = false
+	end
+end
+
+function onPlayerHarm(eventToken)
+	if nokeys then
+		eventToken.cancelled = true
+	end
+end
+
+function onInputUpdate()
+	if nokeys then
+		player.upKeyPressing = false;
+		player.downKeyPressing = false;
+		player.leftKeyPressing = false;
+		player.rightKeyPressing = false;
+		player.altJumpKeyPressing = false;
+		player.runKeyPressing = false;
+		player.altRunKeyPressing = false;
+		player.dropItemKeyPressing = false;
+		player.jumpKeyPressing = false;
+		if Player(2) and Player(2).isValid then
+			player2.upKeyPressing = false;
+			player2.downKeyPressing = false;
+			player2.leftKeyPressing = false;
+			player2.rightKeyPressing = false;
+			player2.altJumpKeyPressing = false;
+			player2.runKeyPressing = false;
+			player2.altRunKeyPressing = false;
+			player2.dropItemKeyPressing = false;
+			player2.jumpKeyPressing = false;
+		end
+	end
+	if nokeysexceptjump then
+		player.upKeyPressing = false;
+		player.downKeyPressing = false;
+		player.leftKeyPressing = false;
+		player.rightKeyPressing = false;
+		player.altJumpKeyPressing = false;
+		player.runKeyPressing = false;
+		player.altRunKeyPressing = false;
+		player.dropItemKeyPressing = false;
+		if Player(2) and Player(2).isValid then
+			player2.upKeyPressing = false;
+			player2.downKeyPressing = false;
+			player2.leftKeyPressing = false;
+			player2.rightKeyPressing = false;
+			player2.altJumpKeyPressing = false;
+			player2.runKeyPressing = false;
+			player2.altRunKeyPressing = false;
+			player2.dropItemKeyPressing = false;
+		end
 	end
 end
