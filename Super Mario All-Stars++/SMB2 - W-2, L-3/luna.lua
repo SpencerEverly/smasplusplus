fb5d15693a9ee61c223dc1aa0a5bd6a19061efd2
Spencer Everly <spencer.everly@gmail.com>
2022-04-21 09:04:42 -0500

    Fixed SMB2 1-3 and 2-3 cutscenes to properly disable player.keys, for only those levels for now

--- Super Mario All-Stars++/SMB2 - W-2, L-3/luna.lua
+++ Super Mario All-Stars++/SMB2 - W-2, L-3/luna.lua
@@ -1,1 +1,49 @@
 local level_dependencies_normal= require("level_dependencies_normal")
+
+local nokeys = false
+
+function onEvent(eventName)
+	if eventName == "Boss End 1" then
+		nokeys = true
+	end
+	if eventName == "Boss End 3" then
+		nokeys = false
+	end
+	if eventName == "World Clear 1" then
+		nokeys = true
+	end
+	if eventName == "World Clear 5" then
+		nokeys = false
+	end
+end
+
+function onPlayerHarm(eventToken)
+	if nokeys then
+		eventToken.cancelled = true
+	end
+end
+
+function onInputUpdate()
+	if nokeys then
+		player.upKeyPressing = false;
+		player.downKeyPressing = false;
+		player.leftKeyPressing = false;
+		player.rightKeyPressing = false;
+		player.altJumpKeyPressing = false;
+		player.runKeyPressing = false;
+		player.altRunKeyPressing = false;
+		player.dropItemKeyPressing = false;
+		player.jumpKeyPressing = false;
+		if Player(2) and Player(2).isValid then
+			player2.upKeyPressing = false;
+			player2.downKeyPressing = false;
+			player2.leftKeyPressing = false;
+			player2.rightKeyPressing = false;
+			player2.altJumpKeyPressing = false;
+			player2.runKeyPressing = false;
+			player2.altRunKeyPressing = false;
+			player2.dropItemKeyPressing = false;
+			player2.jumpKeyPressing = false;
+		end
+	end
+end
