51891d8352ba11421fb4a7964308f3c8787f6b8c
Spencer Everly <spencer.everly@gmail.com>
2022-04-20 19:48:13 -0500

    SMBDDX Mario can duck when in leaf form

--- Super Mario All-Stars++/costumes/mario/SMBDDX-Mario/costume.lua
+++ Super Mario All-Stars++/costumes/mario/SMBDDX-Mario/costume.lua
@@ -111,6 +111,28 @@ local function isSlowFalling()
 	return (leafPowerups[player.powerup] and player.speedY > 0 and (player.keys.jump or player.keys.altJump))
 end
 
+local function canDuck()
+	return (
+		player.forcedState == FORCEDSTATE_NONE
+		and player.deathTimer == 0 and not player:mem(0x13C,FIELD_BOOL) -- not dead
+		and player.mount == MOUNT_NONE
+		and not player.climbing
+		and not player:mem(0x0C,FIELD_BOOL) -- fairy
+		and not player:mem(0x3C,FIELD_BOOL) -- sliding
+		and not player:mem(0x44,FIELD_BOOL) -- surfing on a rainbow shell
+		and not player:mem(0x4A,FIELD_BOOL) -- statue
+		and not player:mem(0x50,FIELD_BOOL) -- spin jumping
+		and player:mem(0x26,FIELD_WORD) == 0 -- picking up something from the top
+		and (player:mem(0x34,FIELD_WORD) == 0 or isOnGround()) -- underwater or on ground
+
+		and (
+			player:mem(0x48,FIELD_WORD) == 0 -- not on a slope (ducking on a slope is weird due to sliding)
+			or (player.holdingNPC ~= nil and player.powerup == PLAYER_SMALL) -- small and holding an NPC
+			or player:mem(0x34,FIELD_WORD) > 0 -- underwater
+		)
+	)
+end
+
 local function isOnGround()
 	return (
 		player.speedY == 0 -- "on a block"
@@ -122,7 +144,7 @@ end
 function costume.onTick()
 	if SaveData.toggleCostumeAbilities == true then
 		if plr.holdingNPC == nil then
-			if isSlowFalling() and not isOnGround() then
+			if isSlowFalling() and not isOnGround() and not player:mem(0x12E, FIELD_BOOL) then
 				plr:playAnim({5,37,11,37}, 4, false, -25)
 			end
 		end
