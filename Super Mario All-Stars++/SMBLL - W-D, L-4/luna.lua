b855d67f96920957f88f9b6c202c3b1a3f94857b
Spencer Everly <spencer.everly@gmail.com>
2022-06-10 15:44:52 -0500

    SMBLL D-4 finished. All of SMBLL is now playable from start to finish!

--- Super Mario All-Stars++/SMBLL - W-D, L-4/luna.lua
+++ Super Mario All-Stars++/SMBLL - W-D, L-4/luna.lua
@@ -1,7 +1,7 @@
 local level_dependencies_normal= require("level_dependencies_normal")
 
 function onEvent(eventName)
-	if eventName == ("Cutscene 4") then
+	if eventName == ("Ending 3") then
 		playSound("pigeon_attack.ogg")
 	end
 end
