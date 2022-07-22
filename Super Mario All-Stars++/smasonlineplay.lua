053ab64e26c0f20d40a595d562873d1ddb16197b
Spencer Everly <spencer.everly@gmail.com>
2022-07-21 21:27:58 -0500

    sending

--- Super Mario All-Stars++/smasonlineplay.lua
+++ Super Mario All-Stars++/smasonlineplay.lua
@@ -35,6 +35,9 @@ local p1coordinatefinal
 local p2coordinates
 local p2coordinatefinal
 
+local p1coordinatessending
+local p2coordinatessending
+
 function smasonlineplay.onDraw()
     if player2Active() then
         if unexpected_condition then
@@ -46,8 +49,8 @@ function smasonlineplay.onDraw()
         if socket.dns.gethostname() == "SPENCERLAPTOP2020" then
             --Player 2 (Sending)
             p2coordinates = player2.x, player2.y
-            assert(udp:send(p2coordinates))
-            if p2coordinates == nil then
+            p2coordinatessending = assert(udp:send(p2coordinates))
+            if p2coordinatessending == nil then
                 Text.print("Not connected.", 100, 100)
             else
                 Text.print(p2coordinates, 100, 100)
@@ -63,8 +66,8 @@ function smasonlineplay.onDraw()
         if socket.dns.gethostname() == "SPENCERPC2022" then
             --Player 1 (Sending)
             p1coordinates = tostring(player.x, player.y)
-            assert(udp2:send(p1coordinates))
-            if p1coordinates == nil then
+            p1coordinatessending = assert(udp2:send(p1coordinates))
+            if p1coordinatessending == nil then
                 Text.print("Not connected.", 100, 100)
             else
                 Text.print(p1coordinates, 100, 100)
