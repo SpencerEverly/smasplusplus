029ad7ea1db5c78b97f5c7dcddf1c8caf5a3d28c
Spencer Everly <spencer.everly@gmail.com>
2022-07-21 22:32:27 -0500

    Adding powerup and frame

--- Super Mario All-Stars++/smasonlineplay.lua
+++ Super Mario All-Stars++/smasonlineplay.lua
@@ -10,17 +10,31 @@ udp:settimeout(0)
 local udp2 = assert(socket.udp())
 udp2:settimeout(0)
 
+local udp3 = assert(socket.udp())
+udp3:settimeout(0)
+
+local udp4 = assert(socket.udp())
+udp4:settimeout(0)
+
 if socket.dns.gethostname() == "SPENCERPC2022" then
     assert(udp:setsockname("*",12345))
     assert(udp:setpeername("25.3.160.51",12345))
     assert(udp2:setsockname("*",12344))
     assert(udp2:setpeername("25.3.160.51",12344))
+    assert(udp3:setsockname("*",12343))
+    assert(udp3:setpeername("25.3.160.51",12343))
+    assert(udp4:setsockname("*",12342))
+    assert(udp4:setpeername("25.3.160.51",12342))
 end
 if socket.dns.gethostname() == "SPENCERLAPTOP2020" then
     assert(udp:setsockname("*",12345))
     assert(udp:setpeername("25.3.161.35",12345))
     assert(udp2:setsockname("*",12344))
     assert(udp2:setpeername("25.3.161.35",12344))
+    assert(udp3:setsockname("*",12343))
+    assert(udp3:setpeername("25.3.161.35",12343))
+    assert(udp4:setsockname("*",12342))
+    assert(udp4:setpeername("25.3.161.35",12342))
 end
 
 function smasonlineplay.onInitAPI()
@@ -31,9 +45,13 @@ end
 
 local p2coordinatex
 local p2coordinatey
+local p2powerup
+local p2frame
 
 local p2coordinatefinalx
 local p2coordinatefinaly
+local p2finalpowerup
+local p2finalframe
 
 smasonlineplay.onlineactivated = false
 
@@ -47,20 +65,31 @@ function smasonlineplay.onDraw()
             if socket.dns.gethostname() == "SPENCERLAPTOP2020" then
                 p2coordinatex = player2.x
                 p2coordinatey = player2.y
+                p2powerup = player2.powerup
+                p2frame = player2.frame
+                
                 assert(udp:send(p2coordinatex))
                 assert(udp2:send(p2coordinatey))
+                assert(udp:send(p2powerup))
+                assert(udp2:send(p2frame))
                 Text.print(inspect(p2coordinates), 100, 120)
             end
             if socket.dns.gethostname() == "SPENCERPC2022" then
                 p2coordinatesfinalx = assert(udp:receive())
                 p2coordinatesfinaly = assert(udp2:receive())
-                if p2coordinatesfinalx == nil or p2coordinatesfinaly == nil then
-                    Text.print("Not connected.", 100, 120)
+                p2finalpowerup = assert(udp3:receive())
+                p2finalframe = assert(udp4:receive())
+                if p2coordinatesfinalx == nil or p2coordinatesfinaly == nil or p2finalpowerup == nil or p2finalframe == nil then
+                    Text.print("Not connected.", 100, 80)
                 else
                     Text.print(p2coordinatesfinalx, 100, 100)
                     Text.print(p2coordinatesfinaly, 100, 120)
+                    Text.print(p2finalpowerup, 100, 140)
+                    Text.print(p2finalframe, 100, 160)
                     player2.x = tonumber(p2coordinatesfinalx)
                     player2.y = tonumber(p2coordinatesfinaly)
+                    player2.powerup = tonumber(p2finalpowerup)
+                    player2.frame = tonumber(p2finalframe)
                 end
             end
         end
