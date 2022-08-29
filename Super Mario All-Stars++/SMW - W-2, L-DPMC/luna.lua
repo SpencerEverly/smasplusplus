8b51ce94e01d24cf0fc0b75b128489643f858f36
Spencer Everly <spencer.everly@gmail.com>
2022-08-29 11:51:57 -0500

    Added better layer movement for SMW Morton's Castle, rest of the level coming soon

--- Super Mario All-Stars++/SMW - W-2, L-DPMC/luna.lua
+++ Super Mario All-Stars++/SMW - W-2, L-DPMC/luna.lua
@@ -1,1 +1,4 @@
 local level_dependencies_normal= require("level_dependencies_normal")
+local slm = require("simpleLayerMovement")
+
+slm.addLayer{name = "Moving Platforms",speed = 75,horizontalMovement = slm.MOVEMENT_COSINE,horizontalSpeed = 482,horizontalDistance = -1}
