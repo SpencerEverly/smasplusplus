7181c27d2b0f5ba527730b97a66001733550144a
Spencer Everly <spencer.everly@gmail.com>
2022-09-09 22:31:23 -0500

    Start on experimental SMBSE Spencer follower code, will be touched on again sometime later

--- Super Mario All-Stars++/smasspencerfollower.lua
+++ Super Mario All-Stars++/smasspencerfollower.lua
@@ -8,10 +8,12 @@ end
 
 function smasspencerfollower.onDraw()
     if not SaveData.disableX2char then
-        if player.direction == 1 then
-            Img.draw("costumes/luigi/00-SpencerEverly/luigi-2.png", player.x - 50, player.y, true, true, -25, 1)
-        else
-            Img.draw("costumes/luigi/00-SpencerEverly/luigi-2.png", player.x + 50, player.y, true, true, -25, 1)
+        if not (SaveData.currentCostume == "00-SPENCEREVERLY") then
+            if player.direction == 1 then
+                --Img.draw("costumes/luigi/00-SpencerEverly/luigi-2.png", player.x - 50, player.y, true, true, -25, 1)
+            else
+                --Img.draw("costumes/luigi/00-SpencerEverly/luigi-2.png", player.x + 50, player.y, true, true, -25, 1)
+            end
         end
     end
 end
