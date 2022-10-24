f08f811ad51d493e12b90e0fea176c1df8ceed22
Spencer Everly <spencer.everly@gmail.com>
2022-10-24 15:30:42 -0500

    Made SMB3 5-9

--- Super Mario All-Stars++/SMB3 - W-5, L-9/luna.lua
+++ Super Mario All-Stars++/SMB3 - W-5, L-9/luna.lua
@@ -1,2 +1,7 @@
 local level_dependencies_normal= require("level_dependencies_normal")
 local inventory = require("customInventory")
+local autoscroll = require("autoscroll")
+
+function onLoadSection0()
+    autoscroll.scrollTo(-197376, -202048, 0.7)
+end
