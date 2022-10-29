da85f55cc6bed08dfe0831888d117e6dae67a59d
Spencer Everly <spencer.everly@gmail.com>
2022-10-29 15:50:43 -0500

    Raise autoscroll speed up 0.3 to make it 1 on SMB3 6-2

--- Super Mario All-Stars++/SMB3 - W-6, L-2/luna.lua
+++ Super Mario All-Stars++/SMB3 - W-6, L-2/luna.lua
@@ -2,5 +2,5 @@ local level_dependencies_normal= require("level_dependencies_normal")
 local autoscroll = require("autoscroll")
 
 function onLoadSection0()
-    autoscroll.scrollRight(0.7)
+    autoscroll.scrollRight(1)
 end
