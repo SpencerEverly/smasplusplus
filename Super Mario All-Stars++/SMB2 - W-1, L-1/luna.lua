63e4dce2d76f0f3667ef27d84273ab054947ca54
Spencer Everly <spencer.everly@gmail.com>
2022-11-07 16:54:55 -0600

    Cleanup on SMB2 stuff related to music changes and sound effects (Everything except for -3 levels so far)

--- Super Mario All-Stars++/SMB2 - W-1, L-1/luna.lua
+++ Super Mario All-Stars++/SMB2 - W-1, L-1/luna.lua
@@ -2,23 +2,25 @@ local level_dependencies_smb2 = require("level_dependencies_normal")
 
 local playerManager = require("playerManager")
 
-local costumes = {}
+local nesCostumes = {
+    "04-SMB2-RETRO",
+    "05-SMB2-RETRO",
+    "03-SMB2-RETRO",
+    "IMAJIN-NES",
+}
+
+local gbaCostumes = {
+    "11-SMA1",
+    "12-SMA2",
+}
 
 function onStart()
     if player:mem(0x15E, FIELD_WORD) == 7 then
         if SaveData.currentCostume == "N/A" then
             Sound.playSFX("smb2-beginning.ogg")
-        elseif SaveData.currentCostume == "04-SMB2-RETRO" then
+        elseif nesCostumes[SaveData.currentCostume] then
             Sound.playSFX("smb1-nes-beginning.ogg")
-        elseif SaveData.currentCostume == "05-SMB2-RETRO" then
-            Sound.playSFX("smb1-nes-beginning.ogg")
-        elseif SaveData.currentCostume == "03-SMB2-RETRO" then
-            Sound.playSFX("smb1-nes-beginning.ogg")
-        elseif SaveData.currentCostume == "IMAJIN-NES" then
-            Sound.playSFX("smb1-nes-beginning.ogg")
-        elseif SaveData.currentCostume == "11-SMA1" then
-            Sound.playSFX("sma1-beginning.ogg")
-        elseif SaveData.currentCostume == "12-SMA2" then
+        elseif gbaCostumes[SaveData.currentCostume] then
             Sound.playSFX("sma1-beginning.ogg")
         elseif SaveData.currentCostume then
             Sound.playSFX("smb2-beginning.ogg")
@@ -30,12 +32,14 @@ function onEvent(eventName)
     if eventName == "Boss Start" then
         Sound.changeMusic("_OST/Super Mario Bros 2/Boss.spc|0;g=2.5", 4)
     end
+    if eventName == "Boss End" then
+        Sound.changeMusic(0, 4)
+        Sound.playSFX(40)
+    end
     if eventName == "Boss End 2" then
         Sound.changeMusic("_OST/Super Mario Bros 2/Boss.spc|0;g=2.5", 4)
     end
-    
-    local costumes
-    if SaveData.currentCostume == "11-SMA1" then
+    --[[if SaveData.currentCostume == "11-SMA1" then
         if eventName == "Boss Start" then
             Sound.playSFX("mario/11-SMA1/birdo-thisisasfarasyougo.wav")
         end
@@ -44,5 +48,5 @@ function onEvent(eventName)
         if eventName == "Boss End" then
             Sound.playSFX("mario/11-SMA1/birdo-I'llrememberthis.wav")
         end
-    end
+    end]]
 end
--- Super Mario All-Stars++/SMB2 - W-7, L-1/luna.lua
+++ Super Mario All-Stars++/SMB2 - W-7, L-1/luna.lua
@@ -1,30 +1,26 @@
 local level_dependencies_normal= require("level_dependencies_normal")
 
---Copy these, they'll be important for changing music throughout rooms. Not copying these WILL throw an "overload" error
-local section0 = 0
-local section1 = 1
-local section2 = 2
-local section3 = 3
-local section4 = 4
-local section5 = 5
-local section6 = 6
-local section7 = 7
-local section8 = 8
-local section9 = 9
-local section10 = 10
-local section11 = 11
-local section12 = 12
-local section13 = 13
-local section14 = 14
-local section15 = 15
-local section16 = 16
-local section17 = 17
-local section18 = 18
-local section19 = 19
-local section20 = 20
-
 function onEvent(eventName)
+    if eventName == "Boss Begin (Part 1)" then
+        Sound.changeMusic("_OST/Super Mario Bros 2/Boss.spc|0;g=2.5", 5)
+    end
+    if eventName == "Boss Dead -1 (Part 0)" then
+        Sound.changeMusic(0, 5)
+        Sound.playSFX(41)
+    end
+    if eventName == "Boss Dead (Part 1)" then
+        Sound.changeMusic(0, 5)
+    end
+    if eventName == "Boss Dead 2 (Part 1)" then
+        Sound.changeMusic("_OST/Super Mario Bros 2/Boss.spc|0;g=2.5", 5)
+    end
+    if eventName == "Boss End (Part 2)" then
+        Sound.changeMusic(0, 5)
+    end
     if eventName == "BossBeginPart 2" then
-        Audio.MusicChange(section5, "_OST/Super Mario Bros 2/King Wart.ogg")
+        Sound.changeMusic("_OST/Super Mario Bros 2/King Wart.ogg", 5)
+    end
+    if eventName == "True Boss End 1" then
+        Sound.playSFX("ace-coins-5.ogg")
     end
 end
