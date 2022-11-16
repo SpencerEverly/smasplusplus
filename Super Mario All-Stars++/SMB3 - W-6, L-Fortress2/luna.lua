b7cca61e7b5e70082437fb7911d650bfdc9a015c
Spencer Everly <spencer.everly@gmail.com>
2022-11-15 19:38:27 -0800

    Finish SMB3 6-Fortress 2

--- Super Mario All-Stars++/SMB3 - W-6, L-Fortress2/luna.lua
+++ Super Mario All-Stars++/SMB3 - W-6, L-Fortress2/luna.lua
@@ -2,9 +2,9 @@ local level_dependencies_normal= require("level_dependencies_normal")
 
 function onEvent(eventName)
     if eventName == "Boss Start" then
-        --Screen.setCameraPosition(-159200,-160600,-160000,-158400,1)
+        Screen.setCameraPosition(-179200,-180600,-180000,-178400,1)
         Sound.playSFX(22)
-        Sound.changeMusic("_OST/Super Mario Bros 3/Boss.spc|0;g=2.5", 2)
+        Sound.changeMusic("_OST/Super Mario Bros 3/Boss.spc|0;g=2.5", 1)
     end
     if eventName == "Boss Start 2" then
         --Sound.playSFX("true-reveal.ogg")
