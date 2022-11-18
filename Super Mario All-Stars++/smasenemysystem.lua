27c7f02167a34dea94365c8aa0ac6a9b5654a850
Spencer Everly <spencer.everly@gmail.com>
2022-11-18 11:19:23 -0600

    Disable fix code for now

--- Super Mario All-Stars++/smasenemysystem.lua
+++ Super Mario All-Stars++/smasenemysystem.lua
@@ -6,20 +6,29 @@ function smasenemysystem.onInitAPI()
     registerEvent(smasenemysystem,"onTick")
 end
 
+local heldNPC
+
+smasenemysystem.enableWallNPCFix = false
+
 function smasenemysystem.onTick()
-    --Disable killing NPCs when throwing them close to walls, making it more like the original Mario games
-    if not SaveData.disableX2char then
-        for k,v in ipairs(NPC.get()) do
-            if v:mem(0x134,FIELD_WORD) >= 5 then --Since NPCs get killed when this is higher than 5...
-                v:mem(0x134,FIELD_WORD,0) --Make it 0 to not kill them
-                local blockOver = Block.getIntersecting(v.x + (16 * v.direction), v.y, v.x + v.width, v.y + v.height) --We still need to move NPCs to prevent a few glitches though, so do an intersecting on the block...
-                --[[if blockOver.x ~= nil then --If this value isn't nil, continue onward...
-                    if blockOver.x < v.x and player.direction == -1 then
-                        v.x = v.x + 16
-                    elseif blockOver.x > v.x and player.direction == 1 then
-                        v.x = v.x - 16
+    if smasenemysystem.enableWallNPCFix then
+        if player.holdingNPC ~= nil then
+            heldNPC = player.holdingNPC
+        end
+        --Disable killing NPCs when throwing them close to walls, making it more like the original Mario games
+        if not SaveData.disableX2char then
+            if heldNPC ~= nil and heldNPC.isValid then
+                if heldNPC:mem(0x134,FIELD_WORD) >= 5 then --Since NPCs get killed when this is higher than 5...
+                    heldNPC:mem(0x134,FIELD_WORD,0) --Make it 0 to not kill them
+                    local blockOver = Block.getIntersecting(heldNPC.x + (16 * heldNPC.direction), heldNPC.y, heldNPC.x + heldNPC.width, heldNPC.y + heldNPC.height) --We still need to move NPCs to prevent a few glitches though, so do an intersecting on the block...
+                    if blockOver.x ~= nil then --If this value isn't nil, continue onward...
+                        if blockOver.x < heldNPC.x and heldNPC.direction == -1 then
+                            heldNPC.speedX = heldNPC.speedX + 3
+                        elseif blockOver.x > heldNPC.x and heldNPC.direction == 1 then
+                            heldNPC.speedX = heldNPC.speedX - 3
+                        end
                     end
-                end]]
+                end
             end
         end
     end
