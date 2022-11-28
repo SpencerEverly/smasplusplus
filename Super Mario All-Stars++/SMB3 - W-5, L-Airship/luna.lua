83472c8caf8a974b94ad365df6bc5a63f09a227c
Spencer Everly <spencer.everly@gmail.com>
2022-11-28 15:18:19 -0600

    Do something new to only show a message when actually starting the level on SMB3 5-Airship, coming onto other Airship levels soon

--- Super Mario All-Stars++/SMB3 - W-5, L-Airship/luna.lua
+++ Super Mario All-Stars++/SMB3 - W-5, L-Airship/luna.lua
@@ -4,6 +4,12 @@ local inventory = require("customInventory")
 local autoscroll = require("autoscroll")
 local airshipScroll = require("airshipScroll")
 
+function onStart()
+    if player:mem(0x15E, FIELD_WORD) == 0 then
+        triggerEvent("Beginning Message 0")
+    end
+end
+
 function onLoadSection2()
     autoscroll.scrollRight(1)
 end
