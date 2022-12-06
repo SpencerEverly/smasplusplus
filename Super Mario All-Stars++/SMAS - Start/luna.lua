9803df8ee2593299da65d406e1eb7357a33a1cfe
Spencer Everly <spencer.everly@gmail.com>
2022-12-06 12:45:00 -0600

    Update boot menu options, now has an erase save data option and everything is much cleaner (Also works on the True Final Boss, but options will be limited)

--- Super Mario All-Stars++/SMAS - Start/luna.lua
+++ Super Mario All-Stars++/SMAS - Start/luna.lua
@@ -16,44 +16,40 @@ bootmenu.active = false
 debugbox.bootactive = false
 GameData.rushModeActive = false
 
-local player2_alt = Player(2)
-
 local active = false
+local timer = 128
+
 local activeselected = 1
 local activeselecty = 32
+
+if SaveData.racaActivated == nil or SaveData.racaActivated == 0 then
+    activeselecty = 32
+elseif SaveData.racaActivated >= 1 then
+    activeselecty = 44
+end
 
 local bootshow = true
 local blackscreen = false
 
 local updateractive = false
 
-onePressedState = false
-twoPressedState = false
-threePressedState = false
-fourPressedState = false
-fivePressedState = false
-sixPressedState = false
-sevenPressedState = false
-eightPressedState = false
-ninePressedState = false
-zeroPressedState = false
+local onePressedState = false
+local twoPressedState = false
+local threePressedState = false
+local fourPressedState = false
+local fivePressedState = false
+local sixPressedState = false
 
-f8PressedState = false
+local f8PressedState = false
 
 GameData.weatherset = true
 GameData.rushModeActive = false
 
 local cooldown = 3
 
-local timer = 128
-local flag = true
-local blackbg = false
-local f8pressed = false
-
 Graphics.activateHud(false)
 
 local Routine = require("routine")
-
 local textplus = require("textplus")
 local littleDialogue = require("littleDialogue")
 
@@ -66,25 +62,27 @@ end
 local function startupdater()
     bootshow = false
     blackscreen = true
-    --Misc.runWhenUnfocused(true)
+    if SMBX_VERSION == VER_SEE_MOD then
+        Misc.runWhenUnfocused(true)
+    end
     Routine.wait(1, true)
     Sound.changeMusic("_OST/All Stars Menu/Updater.ogg", 0)
     updateractive = true
     Routine.wait(5, true)
-    --if Misc.isSetToRunWhenUnfocused() then
-        --Misc.runWhenUnfocused(false)
-    --end
+    if SMBX_VERSION == VER_SEE_MOD then
+        if Misc.isSetToRunWhenUnfocused() then
+            Misc.runWhenUnfocused(false)
+        end
+    end
     Routine.wait(0.1, true)
     SysManager.loadIntroTheme()
 end
 
 function onKeyboardPress(k, v)
     if not updateractive and not blackscreen then
-        if SaveData.racaActivated == nil or SaveData.racaActivated == 0 then
-            if k == VK_F8 then
-                f8PressedState = true
-                active = true
-            end
+        if k == VK_F8 then
+            f8PressedState = true
+            active = true
         end
         if active then
             if k == VK_F8 then
@@ -94,11 +92,6 @@ function onKeyboardPress(k, v)
                 fourPressedState = false
             end
         end
-        if not active then
-            if k == VK_F8 then
-                f8PressedState = true
-            end
-        end
     end
 end
 
@@ -106,32 +99,54 @@ function onDraw()
     local bootimage = Graphics.loadImageResolved("SMAS - Start/bootscreen_final.png")
     
     if bootshow then
-        Graphics.drawImageWP(bootimage, 0, 0, -2)
+        Graphics.drawImageWP(bootimage, 0, 0, -4)
     end
     if blackscreen then
         Graphics.drawScreen{color = Color.black, priority = -5}
     end
     if active then
-        if cooldown > 0 then
-            cooldown = cooldown - 1
-        end
-        Graphics.drawScreen{color = Color.black, priority = -1}
-        textplus.print{x=40, y=10, text = "Super Mario All-Stars++ Temporary Boot Option List", priority=0, color=Color.white}
-        textplus.print{x=40, y=32, text = "1) Reset character", priority=0, color=Color.white}
-        textplus.print{x=40, y=44, text = "2) Reset the main menu theme", priority=0, color=Color.white}
-        textplus.print{x=40, y=56, text = "3) Reset profile picture and player name", priority=0, color=Color.white}
-        textplus.print{x=40, y=68, text = "4) Continue booting", priority=0, color=Color.white}
-        textplus.print{x=40, y=80, text = "5) Load world map instantly (With default settings on first-boot)", priority=0, color=Color.white}
-        textplus.print{x=40, y=92, text = "6) Enable speedrun mode", priority=0, color=Color.white}
-        
-        textplus.print{x=20, y=activeselecty, plaintext = true, text = "->", priority=0, color=Color.white}
-        
-        if activeselected < 1 then
-            activeselecty = 92
-            activeselected = 6
-        elseif activeselected > 6 then
-            activeselecty = 32
-            activeselected = 1
+        if SaveData.racaActivated == nil or SaveData.racaActivated == 0 then
+            if cooldown > 0 then
+                cooldown = cooldown - 1
+            end
+            Graphics.drawScreen{color = Color.black, priority = -2}
+            textplus.print{x=40, y=10, text = "Super Mario All-Stars++ Temporary Boot Option List", priority=-1, color=Color.white}
+            textplus.print{x=40, y=32, text = "1) Continue booting", priority=-1, color=Color.white}
+            textplus.print{x=40, y=44, text = "2) Load world map instantly (With default settings on first boot)", priority=-1, color=Color.white}
+            textplus.print{x=40, y=56, text = "3) Enable speedrun mode", priority=-1, color=Color.white}
+            textplus.print{x=40, y=68, text = "4) Reset character", priority=-1, color=Color.white}
+            textplus.print{x=40, y=80, text = "5) Reset the main menu theme", priority=-1, color=Color.white}
+            textplus.print{x=40, y=92, text = "6) Reset profile picture and player name", priority=-1, color=Color.white}
+            textplus.print{x=40, y=104, text = "7) Erase save data (NO WARNING WILL SHOW UP!)", priority=-1, color=Color.white}
+            
+            textplus.print{x=20, y=activeselecty, plaintext = true, text = "->", priority=-1, color=Color.white}
+            
+            if activeselected < 1 then
+                activeselecty = 104
+                activeselected = 7
+            elseif activeselected > 7 then
+                activeselecty = 32
+                activeselected = 1
+            end
+        elseif SaveData.racaActivated >= 1 then
+            if cooldown > 0 then
+                cooldown = cooldown - 1
+            end
+            Graphics.drawScreen{color = Color.black, priority = -2}
+            textplus.print{x=40, y=10, text = "Super Mario All-Stars++ Temporary Boot Option List", priority=-1, color=Color.white}
+            textplus.print{x=40, y=32, text = "(Options are limited during the True Final Battle)", priority=-1, color=Color.white}
+            textplus.print{x=40, y=44, text = "1) Continue booting", priority=-1, color=Color.white}
+            textplus.print{x=40, y=56, text = "2) Reset save data (NO WARNING WILL SHOW UP!)", priority=-1, color=Color.white}
+            
+            textplus.print{x=20, y=activeselecty, plaintext = true, text = "->", priority=-1, color=Color.white}
+            
+            if activeselected < 1 then
+                activeselecty = 56
+                activeselected = 2
+            elseif activeselected > 2 then
+                activeselecty = 44
+                activeselected = 1
+            end
         end
     end
     if updateractive then
@@ -159,12 +174,10 @@ function onInputUpdate()
         player2.dropItemKeyPressing = false
     end
     if not updateractive and not blackscreen then
-        if SaveData.racaActivated == nil or SaveData.racaActivated == 0 then
-            if player.keys.jump == KEYS_PRESSED then
-                f8PressedState = true
-                active = true
-                player:mem(0x11E, FIELD_BOOL, false)
-            end
+        if player.keys.jump == KEYS_PRESSED then
+            f8PressedState = true
+            active = true
+            player:mem(0x11E, FIELD_BOOL, false)
         end
     end
     if active then
@@ -176,51 +189,81 @@ function onInputUpdate()
             activeselecty = activeselecty - 12
         end
         if cooldown == 0 then
-            if player.keys.jump == KEYS_PRESSED then
-                if activeselected == 1 then
-                    player.setCostume(1, nil)
-                    player.setCostume(2, nil)
-                    player.setCostume(3, nil)
-                    player.setCostume(4, nil)
-                    player.setCostume(5, nil)
-                    player.setCostume(6, nil)
-                    player.setCostume(7, nil)
-                    player.setCostume(8, nil)
-                    player.setCostume(9, nil)
-                    player.setCostume(10, nil)
-                    player.setCostume(11, nil)
-                    player.setCostume(12, nil)
-                    player.setCostume(13, nil)
-                    player.setCostume(14, nil)
-                    player.setCostume(15, nil)
-                    player.setCostume(16, nil)
-                    player:transform(CHARACTER_MARIO, false)
-                    SysManager.loadIntroTheme()
-                end
-                if activeselected == 2 then
-                    SaveData.introselect = 1
-                    Level.load("intro_SMAS.lvlx")
-                end
-                if activeselected == 3 then
-                    SaveData.playerName = nil
-                    SaveData.playerPfp = nil
-                    SysManager.loadIntroTheme()
-                end
-                if activeselected == 4 then
-                    SysManager.loadIntroTheme()
-                end
-                if activeselected == 5 then
-                    GameData.____mainMenuComplete = true
-                    Level.load("map.lvlx")
-                end
-                if activeselected == 6 then
-                    GameData.____mainMenuComplete = true
-                    SaveData.speedrunMode = not SaveData.speedrunMode
-                    if not SaveData.firstBootCompleted then
-                        SaveData.firstBootCompleted = true
+            if SaveData.racaActivated == nil or SaveData.racaActivated == 0 then
+                if player.keys.jump == KEYS_PRESSED then
+                    if activeselected == 1 then
+                        SysManager.loadIntroTheme()
                     end
-                    Misc.saveGame()
-                    Level.load("map.lvlx")
+                    if activeselected == 2 then
+                        GameData.____mainMenuComplete = true
+                        Level.load("map.lvlx")
+                    end
+                    if activeselected == 3 then
+                        GameData.____mainMenuComplete = true
+                        SaveData.speedrunMode = not SaveData.speedrunMode
+                        if not SaveData.firstBootCompleted then
+                            SaveData.firstBootCompleted = true
+                        end
+                        Misc.saveGame()
+                        Level.load("map.lvlx")
+                    end
+                    if activeselected == 4 then
+                        player.setCostume(1, nil)
+                        player.setCostume(2, nil)
+                        player.setCostume(3, nil)
+                        player.setCostume(4, nil)
+                        player.setCostume(5, nil)
+                        player.setCostume(6, nil)
+                        player.setCostume(7, nil)
+                        player.setCostume(8, nil)
+                        player.setCostume(9, nil)
+                        player.setCostume(10, nil)
+                        player.setCostume(11, nil)
+                        player.setCostume(12, nil)
+                        player.setCostume(13, nil)
+                        player.setCostume(14, nil)
+                        player.setCostume(15, nil)
+                        player.setCostume(16, nil)
+                        player:transform(CHARACTER_MARIO, false)
+                        SysManager.loadIntroTheme()
+                    end
+                    if activeselected == 5 then
+                        SaveData.introselect = 1
+                        Level.load("intro_SMAS.lvlx")
+                    end
+                    if activeselected == 6 then
+                        SaveData.playerName = nil
+                        SaveData.playerPfp = nil
+                        SysManager.loadIntroTheme()
+                    end
+                    if activeselected == 7 then
+                        Misc.eraseSaveSlot(Misc.saveSlot())
+                        Misc.dialog("There WILL be a few errors before restarting. This is normal. Just click okay (Or press ENTER) on all dialog boxes that pop up, and the episode will restart.")
+                        SaveData.clear()
+                        GameData.clear()
+                        SaveData.flush()
+                        if Misc.loadEpisode("Super Mario All-Stars++") == false then
+                            SFX.play("wrong.ogg")
+                            error("SMAS++ is not found. How is that even possible? Reinstall the game using the SMASUpdater, since something has gone terribly wrong.")
+                        end
+                    end
+                end
+            elseif SaveData.racaActivated >= 1 then
+                if player.keys.jump == KEYS_PRESSED then
+                    if activeselected == 1 then
+                        SysManager.loadIntroTheme()
+                    end
+                    if activeselected == 2 then
+                        Misc.eraseSaveSlot(Misc.saveSlot())
+                        Misc.dialog("There WILL be a few errors before restarting. This is normal. Just click okay (Or press ENTER) on all dialog boxes that pop up, and the episode will restart.")
+                        SaveData.clear()
+                        GameData.clear()
+                        SaveData.flush()
+                        if Misc.loadEpisode("Super Mario All-Stars++") == false then
+                            SFX.play("wrong.ogg")
+                            error("SMAS++ is not found. How is that even possible? Reinstall the game using the SMASUpdater, since something has gone terribly wrong.")
+                        end
+                    end
                 end
             end
         end
