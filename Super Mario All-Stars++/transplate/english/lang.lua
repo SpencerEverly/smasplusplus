4f582085a9d1a4c583e80ea494371aee7ba2f5e2
Spencer Everly <spencer.everly@gmail.com>
2022-12-07 20:46:58 -0600

    Start on a new system idea for the language system...?

--- Super Mario All-Stars++/transplate/english/lang.lua
+++ Super Mario All-Stars++/transplate/english/lang.lua
@@ -1,3 +1,5 @@
 return{
-    [""] = ""
+    --SMAS++ MAIN MENU
+    --First Boot
+    ["0x0000000000000001"] = "<setPos 400 32 0.5 -0.7>Welcome to Super Mario All-Stars++.<page>This game combines Super Mario Bros. 1-3, The Lost Levels, World,<page>And also includes a new game, along with extra content.<page>Please note that this is an Open Source project.<page>So please understand that BUGS may occur! Don't hesitate to report them on the GitHub page.<page>You can find it at https://github.com/SpencerEverly/smasplusplus/.<page>Other than that, please enjoy the game!<page>But, before we get started, this game needs to set up some prerequisite options.<question FirstBootMenuOne>"
 }
